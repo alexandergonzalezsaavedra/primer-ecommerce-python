a39bfc34c00dc5e3b79913ebec7ffc56841e81fc
Alexander Gonzalez <alexandergonzalezsaavedra@gmail.com>
2023-06-14 07:53:44 -0500

    ajuste viewa.py - url.py

--- orders/urls.py
+++ orders/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     re_path(r"^getcategories$", categoryListApi.as_view(), name="getcategories"),
     re_path(r"^getsubcategories$", subcategoryListApi.as_view(), name="getsubcategories"),
     re_path(r"^getproducts$", productListApi.as_view(), name="getproducts"),
-    # re_path(r'^productdetail/<int:pk>/', ProductDetailView.as_view(), name='productDetail'),
     re_path(r'^productDetail/(?P<pk>\d+)/$', ProductDetailView.as_view(), name='productDetail'),
+#   re_path(r'^productdetail/<int:pk>/', ProductDetailView.as_view(), name='productDetail') anteriores versiones,
 ]
